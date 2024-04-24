098dfe079a308bbb82f6f4b8b0fbd039a6594ca1
Knight <77844703+nightcoder26@users.noreply.github.com>
2024-04-24 23:47:26 +0530

    update

--- mainpage.js
+++ mainpage.js
@@ -12,45 +12,45 @@ document.addEventListener("DOMContentLoaded", function () {
         });
 
 function redirectToRestaurantWebsite() {
-window.location.href = '/Users/sanjaychariteshmakam/Visual-Studio/Web project/index.html'; 
+window.location.href = 'index.html'; 
 }                        
 
 
     document.getElementById("viewAllButton").addEventListener("click", function() {
-        window.location.href = "/Users/sanjaychariteshmakam/Visual-Studio/Web project/web-foods-cards-deserts.html";
+        window.location.href = "web-foods-cards-deserts.html";
     });
 
 
     function indianBreads() {
-        window.location.href = '/Users/sanjaychariteshmakam/Visual-Studio/Web project/web-foods-cards-salads.html'; 
+        window.location.href = 'web-foods-cards-salads.html'; 
         } 
 
         function desserts() {
-            window.location.href = '/Users/sanjaychariteshmakam/Visual-Studio/Web project/web-foods-cards-deserts.html'; 
+            window.location.href = 'web-foods-cards-deserts.html'; 
             } 
         
             function mainCourse() {
-                window.location.href = '/Users/sanjaychariteshmakam/Visual-Studio/Web project/web-foods-cards-main-course.html'; 
+                window.location.href = 'web-foods-cards-main-course.html'; 
                 } 
 
                 function nonVegCurries() {
-                    window.location.href = '/Users/sanjaychariteshmakam/Visual-Studio/Web project/web-foods-cards-noodles.html'; 
+                    window.location.href = 'web-foods-cards-noodles.html'; 
                     } 
 
                     function vegCurries() {
-                        window.location.href = '/Users/sanjaychariteshmakam/Visual-Studio/Web project/web-foods-cards-sea-foods.html'; 
+                        window.location.href = 'web-foods-cards-sea-foods.html'; 
                         } 
                     
                         function soups() {
-                            window.location.href = '/Users/sanjaychariteshmakam/Visual-Studio/Web project/web-foods-cards-soups.html'; 
+                            window.location.href = 'web-foods-cards-soups.html'; 
                             } 
 
                             function vegStarters() {
-                                window.location.href = '/Users/sanjaychariteshmakam/Visual-Studio/Web project/web-foods-cards-veg-starters.html'; 
+                                window.location.href = 'web-foods-cards-veg-starters.html'; 
                                 } 
 
                                 function nonVegStarters() {
-                                    window.location.href = '/Users/sanjaychariteshmakam/Visual-Studio/Web project/web-foods-cards.html'; 
+                                    window.location.href = 'web-foods-cards.html'; 
                                     } 
 
 
